00d367a1edb78e29a80be61b3dbd401e3258e6ff
MaxYashch <maxyashch@gmail.com>
2020-05-03 12:36:22 +0300

    deleted p and span tags where redundant

--- js/accordion.js
+++ js/accordion.js
@@ -4,10 +4,23 @@ for (var i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
+    if (panel.style.display === "none") {
       panel.style.display = "block";
+    } else {
+      panel.style.display = "none";
     }
   });
 }
+
+// accordion-content
+
+
+
+
+// if (panel.style.display === "block") {
+//   panel.style.display = "none";
+// } else {
+//   panel.style.display = "block";
+// }
+// this.classList.toggle("form-body__form-item_active");
+// this.classList.toggle("form-body__item-button_active");
